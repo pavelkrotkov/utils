32de724599b65ace28eeccc5a28832a2db8d3998
pavelkrotkov <41207003+pavelkrotkov@users.noreply.github.com>
2026-06-10 14:30:02 -0400

    Scaffold TranscriptionLauncher Swift package (#74)

---
+++ TranscriptionLauncher/Sources/TranscriptionLauncherLib/TranscriptionLauncherLib.swift
@@ -0,0 +1,7 @@
+public struct AppMetadata: Equatable {
+    public let displayName: String
+
+    public init(displayName: String = "Transcription Launcher") {
+        self.displayName = displayName
+    }
+}
