093e77d0abd1df6298c0ebd3773c28022da57f62
Pavel Krotkov <pavel@imac.lan>
2026-06-10 09:07:28 -0400

    Exercise TranscriptionLauncher library in test

--- TranscriptionLauncher/Sources/TranscriptionLauncherLib/TranscriptionLauncherLib.swift
+++ TranscriptionLauncher/Sources/TranscriptionLauncherLib/TranscriptionLauncherLib.swift
@@ -1,1 +1,7 @@
-public enum TranscriptionLauncherLib {}
+public struct AppMetadata: Equatable {
+    public let displayName: String
+
+    public init(displayName: String = "Transcription Launcher") {
+        self.displayName = displayName
+    }
+}
